4f34682098d346426851eca8e6638228818e14aa
Per Lindgren <per.lindgren@ltu.se>
2023-03-18 09:35:54 +0100

    software interrupts

--- examples/software_interrupt.rs
+++ examples/software_interrupt.rs
@@ -1,7 +1,7 @@
-//! GPIO interrupt
+//! Software Interrupt
 //!
-//! This prints "Interrupt" when the boot button is pressed.
-//! It also blinks an LED like the blinky example.
+//! Software interrupts 0..3 are triggered one by one
+//! with 500ms interval in an endless loop.
 
 #![no_std]
 #![no_main]
@@ -11,19 +11,17 @@ use core::cell::RefCell;
 use critical_section::Mutex;
 use esp32c3_hal::{
     clock::ClockControl,
-    gpio::{Event, Gpio9, Input, PullDown, IO},
     interrupt::{self},
     peripherals::{self, Peripherals},
     prelude::*,
     riscv,
+    system::{SoftwareInterrupt, SoftwareInterruptControl},
     timer::TimerGroup,
     Delay, Rtc,
 };
-use esp_hal_common::system::{SoftwareInterrupt, SoftwareInterruptControl};
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
-static BUTTON: Mutex<RefCell<Option<Gpio9<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
 static SWINT: Mutex<RefCell<Option<SoftwareInterruptControl>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
@@ -48,19 +46,8 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
 
-    rprintln!("here");
-
-    // Set GPIO7 as an output connected to the LED
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let mut led = io.pins.gpio7.into_push_pull_output();
-
-    // Set GPIO9 as an input connected to the "boot" button
-    let mut button = io.pins.gpio9.into_pull_down_input();
-    button.listen(Event::FallingEdge);
-    critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).replace(button));
     critical_section::with(|cs| SWINT.borrow_ref_mut(cs).replace(sw_int));
 
-    interrupt::enable(peripherals::Interrupt::GPIO, interrupt::Priority::Priority3).unwrap();
     interrupt::enable(
         peripherals::Interrupt::FROM_CPU_INTR0,
         interrupt::Priority::Priority3,
@@ -81,14 +68,10 @@ fn main() -> ! {
         interrupt::Priority::Priority3,
     )
     .unwrap();
-    unsafe {
-        riscv::interrupt::enable();
-    }
+    unsafe { riscv::interrupt::enable() }
     let mut delay = Delay::new(&clocks);
     let mut counter = 0;
     loop {
-        led.toggle().unwrap();
-
         delay.delay_ms(500u32);
         match counter {
             0 => critical_section::with(|cs| {
@@ -112,31 +95,23 @@ fn main() -> ! {
                     .unwrap()
                     .raise(SoftwareInterrupt::SoftwareInterrupt2);
             }),
-            3 => critical_section::with(|cs| {
-                SWINT
-                    .borrow_ref_mut(cs)
-                    .as_mut()
-                    .unwrap()
-                    .raise(SoftwareInterrupt::SoftwareInterrupt3);
-            }),
-
+            3 => {
+                critical_section::with(|cs| {
+                    SWINT
+                        .borrow_ref_mut(cs)
+                        .as_mut()
+                        .unwrap()
+                        .raise(SoftwareInterrupt::SoftwareInterrupt3);
+                });
+                counter = 0;
+                continue;
+            }
             _ => {}
         }
-        counter = (counter + 1) % 4;
+        counter += 1;
     }
 }
 
-#[interrupt]
-fn GPIO() {
-    critical_section::with(|cs| {
-        rprintln!("GPIO interrupt");
-        BUTTON
-            .borrow_ref_mut(cs)
-            .as_mut()
-            .unwrap()
-            .clear_interrupt();
-    });
-}
 #[interrupt]
 fn FROM_CPU_INTR0() {
     rprintln!("SW interrupt0");
