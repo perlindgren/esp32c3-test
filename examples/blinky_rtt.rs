2eacef1c14ed29ec7c0934c80e6ea41f36b5f08d
Per Lindgren <per.lindgren@ltu.se>
2023-09-24 15:09:00 +0200

    blinky_rtt

--- examples/blinky_rtt.rs
+++ examples/blinky_rtt.rs
@@ -10,10 +10,7 @@
 #![no_std]
 #![no_main]
 
-use esp32c3_hal::{
-    clock::ClockControl, gpio::IO, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay,
-    Rtc,
-};
+use esp32c3_hal::{clock::ClockControl, gpio::IO, peripherals::Peripherals, prelude::*, Delay};
 
 use rtt_target::{rprintln, rtt_init_print};
 
@@ -22,36 +19,26 @@ use panic_rtt_target as _;
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
+    rprintln!("blinky_rtt");
+
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    // Disable the watchdog timers. For the ESP32-C3, this includes the Super WDT,
-    // the RTC WDT, and the TIMG WDTs.
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-    let mut wdt1 = timer_group1.wdt;
-
-    rtc.swd.disable();
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
-
-    rprintln!("main");
     // Set GPIO7 as an output, and set its state high initially.
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut led = io.pins.gpio7.into_push_pull_output();
 
     led.set_high().unwrap();
 
-    // Initialize the Delay peripheral, and use it to toggle the LED state in a
-    // loop.
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a loop.
     let mut delay = Delay::new(&clocks);
 
+    let mut i = 0;
+
     loop {
-        rprintln!("blink");
+        i = (i + 1) % 10;
+        rprintln!("blink {}", i);
 
         led.toggle().unwrap();
         delay.delay_ms(500u32);
